61d67644ec4f633927f26f330b8a3237f9c53679
Pedro Sampaio <sampaio.pedro@gmail.com>
2020-09-26 23:20:44 +0100

    Analytics ao executar a comparacao

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Blazor.Analytics;
+using Blazor.Analytics.GoogleAnalytics;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,12 +15,14 @@ namespace FlatTaxPT
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(
+                sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
-            if (builder.HostEnvironment.IsProduction())
-            {
-                builder.Services.AddGoogleAnalytics("UA-179107737-1");
-            }
+            var trackingId = "UA-179107737-1";
+            builder.Services.AddScoped<IAnalytics>(p =>
+                new ProductionOnlyAnalytics(
+                    ActivatorUtilities.CreateInstance<GoogleAnalyticsStrategy>(p),
+                    builder.HostEnvironment, trackingId));
 
             builder.Services.AddSingleton<ICalculadorImpostosProgressivos, CalculadorImpostosProgressivos>();
             builder.Services.AddSingleton<ICalculadorImpostosFlat, CalculadorImpostosFlat>();
