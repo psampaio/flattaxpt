1b73a0e9db5c016094b15f503b3115f213849172
Pedro Sampaio <sampaio.pedro@gmail.com>
2022-01-10 15:53:07 +0000

    Criei novo projeto para extrair as tabelas de Excel

---
+++ FlatTaxPT.ExtracaoTabelas/Program.cs
@@ -0,0 +1,13 @@
+﻿using ClosedXML.Excel;
+
+const string ficheiroTabelas = "TabelasRetencao.xlsx";
+
+Console.WriteLine("FlatTaxPT: Extração de Tabelas de Retenção");
+
+Console.WriteLine("A abrir o ficheiro...");
+
+var workbook = new XLWorkbook(ficheiroTabelas);
+
+//var worksheet = workbook.Worksheet("Data");
+
+workbook.Dispose();
