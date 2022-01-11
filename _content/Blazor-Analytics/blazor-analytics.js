2ec18d5f0745d31dca506c6ecdeabfc60a862719
psampaio <sampaio.pedro@gmail.com>
2022-01-11 15:52:41 +0000

    Deploying to gh-pages from  @ 9384a0912912f1184a50ba5406e8c44388eaeda7 🚀

--- _content/Blazor-Analytics/blazor-analytics.js
+++ _content/Blazor-Analytics/blazor-analytics.js
@@ -3,23 +3,31 @@ window.gtag = window.gtag || function () { dataLayer.push(arguments); };
 gtag("js", new Date());
 var GoogleAnalyticsInterop;
 (function (GoogleAnalyticsInterop) {
-    function configure(trackingId) {
+    function configure(trackingId, debug) {
+        if (debug === void 0) { debug = false; }
+        this.debug = debug;
         var script = document.createElement("script");
         script.async = true;
         script.src = "https://www.googletagmanager.com/gtag/js?id=" + trackingId;
         document.head.appendChild(script);
-        gtag("config", trackingId);
-        console.log("[GTAG][" + trackingId + "] Configured!");
+        gtag("config", trackingId, { 'send_page_view': false });
+        if (this.debug) {
+            console.log("[GTAG][" + trackingId + "] Configured!");
+        }
     }
     GoogleAnalyticsInterop.configure = configure;
     function navigate(trackingId, href) {
         gtag("config", trackingId, { page_location: href });
-        console.log("[GTAG][" + trackingId + "] Navigated: '" + href + "'");
+        if (this.debug) {
+            console.log("[GTAG][" + trackingId + "] Navigated: '" + href + "'");
+        }
     }
     GoogleAnalyticsInterop.navigate = navigate;
-    function trackEvent(event, eventCategory, eventLabel, eventValue) {
-        gtag("event", event, { event_category: eventCategory, event_label: eventLabel, value: eventValue });
-        console.log("[GTAG][Event trigered]: " + event);
+    function trackEvent(eventName, eventData) {
+        gtag("event", eventName, eventData);
+        if (this.debug) {
+            console.log("[GTAG][Event triggered]: " + eventName);
+        }
     }
     GoogleAnalyticsInterop.trackEvent = trackEvent;
 })(GoogleAnalyticsInterop || (GoogleAnalyticsInterop = {}));
