e630d6d251658eaff6a3491cb78f93b2ca08fddf
Pedro Sampaio <sampaio.pedro@gmail.com>
2022-01-12 16:38:35 +0000

    Retencao para trabalho dependente arredonda

--- FlatTaxPT/TaxSummary.cs
+++ FlatTaxPT/TaxSummary.cs
@@ -6,6 +6,6 @@ public class TaxSummary
     public decimal Taxable { get; init; }
     public decimal Deduction { get; init; }
     public decimal Rate { get; set; }
-    public decimal Retention => Taxable * Rate;
+    public decimal Retention => Math.Floor(Taxable * Rate);
     public decimal NetIncome => BaseIncome - Retention;
 }
